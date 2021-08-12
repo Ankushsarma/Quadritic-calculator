de8a822e6583ea0664b672cfc4e07010c37376c1
Ankush Sharma <ankushsharma5524@gmail,com>
2021-08-12 23:44:39 +0530

    "answer correction"

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ app.post("/" , function(req,res){
     const Fourac= 4*a*c;
 
     const discriminant = bb - Fourac;
-    const squarerootofDiscriminant=Number(Math.sqrt(discriminant));
+    const squarerootofDiscriminant=(Math.sqrt(discriminant));
   
-    var sq = Number(Math.floor(Math.sqrt(squarerootofDiscriminant)))
-
+    var sq = (Math.floor(squarerootofDiscriminant))
+    console.log(sq);
+    //  perfect square 
     if(sq *sq == discriminant ){
-        console.log("ps");
-        const numerator1 = Number(-1 * b) + squarerootofDiscriminant;
+        const numerator1 = (-1 * b) + squarerootofDiscriminant;
         const numerator2 = -b - squarerootofDiscriminant;
         const denominator = 2 * a;
 
@@ -36,6 +36,7 @@ app.post("/" , function(req,res){
 
         const NdidD02 = numerator2 / denominator;
 
+        //  divisable 
         if(NdidD01 * denominator == numerator1 & NdidD02 * denominator == numerator2 ){
             
             if(discriminant > 0 ) {
@@ -44,12 +45,25 @@ app.post("/" , function(req,res){
             
             }else if ( discriminant == 0) {
                 // res.send("<h1> a2 the roots of the given equation is " + NdidD01 + " and " + NdidD02 + " 2");
-                res.render(__dirname + '/answer', {  answer: "  the roots of the given equation is " + NdidD01 + " and " + NdidD02   })
+                res.render(__dirname + '/answer', {  answer: " the roots of the given equation is " + NdidD01 + " and " + NdidD02   })
             } 
             else if( discriminant < 0){
                 // res.send("no real value")
-                res.render(__dirname + '/answer', {  answer: "no real value"  });
+                const numerator1 =  -b + " + " +  squarerootofDiscriminant + "i";
+                const numerator2 =  -b + " - " +  squarerootofDiscriminant + "i";
+
+                const denominator = 2*a
+
+                const root01 = numerator1 + " / " +denominator;
+                const root02 = numerator2 + " / " +denominator;
+
+
+
+                // res.send("no real value")
+               res.render(__dirname + '/answer', {  answer: "The roots of the given equation is " + root01 + " and " + root02   })
             }
+
+        //  not divisable  
         }else{
             const numerator1 =  -b +  squarerootofDiscriminant;
             const numerator2 =  -b -  squarerootofDiscriminant;
@@ -59,92 +73,79 @@ app.post("/" , function(req,res){
             const root01 = numerator1 + " / " +denominator;
             const root02 = numerator2 + " / " +denominator;
 
+            if(discriminant > 0 ) {
+                // res.send("<h1> a1 the roots of the given equation is " + NdidD01 + " and " + NdidD02 +  " 1");
+                res.render(__dirname + '/answer', {  answer: "The roots of the given equation is " + root01 + " and " + root02  })
+            
+            }else if ( discriminant == 0) {
+                // res.send("<h1> a2 the roots of the given equation is " + NdidD01 + " and " + NdidD02 + " 2");
+                res.render(__dirname + '/answer', {  answer: "The roots of the given equation is " + root01 + " and " + root02   })
+            } else {
+                const numerator1 =  -b + " + " +  squarerootofDiscriminant + "i";
+                const numerator2 =  -b + " - " +  squarerootofDiscriminant + "i";
+
+                const denominator = 2*a
+
+                const root01 = numerator1 + " / " +denominator;
+                const root02 = numerator2 + " / " +denominator;
+
+
+
+                // res.send("no real value")
+               res.render(__dirname + '/answer', {  answer: "The roots of the given equation is " + root01 + " and " + root02   })
+            }
+
             // res.send("<h1> a3 The root of the given equation is " + root01 +" & " + root02 );
-            res.render(__dirname + '/answer', {  answer: "  The root of the given equation is " + root01 +" & " + root02  });
+            // res.render(__dirname + '/answer', {  answer: "  The root of the given equation is " + root01 +" & " + root02  });
         }
 
         
 
         
  
-      
+    //   not perfect square 
     }else {
         if(discriminant > 0){
             const numerator1 = "( "+ -b + " + √" + discriminant +" ) ";
-        const denominator = 2*a
+            const denominator = 2*a
 
-        const numerator2 = "( "+ -b + " - √" + discriminant +" ) ";
+            const numerator2 = "( "+ -b + " - √" + discriminant +" ) ";
         
 
-        const root01 = numerator1 + " / "+ denominator;
-        const root02 = numerator2 + " / "+ denominator;
+            const root01 = numerator1 + " / "+ denominator;
+            const root02 = numerator2 + " / "+ denominator;
 
         // res.send("<h1> a4 The root of the equation is " + root01 +" and " + root02 +"</h1>" )
-        res.render(__dirname + '/answer', {  answer:  " The root of the equation is " + root01 +" and " + root02  });
+        res.render(__dirname + '/answer', {  answer:  "The root of the equation is " + root01 +" and " + root02  });
 
         }else if(discriminant == 0) {
             const numerator1 = "( "+ -b + " + √" + discriminant +" ) ";
-        const denominator = 2*a
+            const denominator = 2*a
 
-        const numerator2 = "( "+ -b + " - √" + discriminant +" ) ";
+            const numerator2 = "( "+ -b + " - √" + discriminant +" ) ";
         
 
-        const root01 = numerator1 + " / "+ denominator;
-        const root02 = numerator2 + " / "+ denominator;
+          const root01 = numerator1 + " / "+ denominator;
+          const root02 = numerator1 + " / "+ denominator;
 
         // res.send("<h1> a5 The root of the equation is " + root01 +" and " + root02 +"</h1>" )
         res.render(__dirname + '/answer', {  answer:  "The root of the equation is " + root01 +" and " + root02   });
         }else{
             // res.send("<h1>a6  no real value </h1>");
-            res.render(__dirname + '/answer', {  answer:  "  no real value "  });
+            // res.render(__dirname + '/answer', {  answer:  "  no real value "  });
+            const numerator1 = "( "+ -b + " + √" + (discriminant * -1) +"i ) ";
+            const numerator2 = "( "+ -b + " - √" + (discriminant * -1) +"i ) ";
+            const denominator = 2*a
+
+            const root01 = numerator1 + " / "+ denominator;
+            const root02 = numerator2 + " / "+ denominator;
+            res.render(__dirname + '/answer', {  answer:  "The root of the equation is " + root01 +" and " + root02  });
         }
 
         
       
     }
 
-  
-
-  
-    // 
-
-    // var no1 = numerator1 / denominator;
-    // var no2 = numerator2 / denominator;
-
-    // var result01 = Math.floor(no1);
-    // var result02 = Math.floor(no2);
-
-
-    // if(result01 * denominator == numerator1){
-    //     var root01 = no1
-    // }else {
-    //     var root01 = numerator1 + " / " + denominator
-        
-    // }
-
-    // if(result02 * denominator == numerator2){
-    //     var root02 = no2
-    // }else {
-    //     var root02 = numerator2 + " / " + denominator
-        
-    // }
-
-    // console.log(root01 );
-    // console.log(root02 );
-    
-
- 
-    // if(discriminant > 0){
-    //     console.log("real and different roots");
-    //     const root1 = String((-bb - squarerootofDiscriminant)/ (2 * a)) ;
-    //     const root2 =String( (-bb + squarerootofDiscriminant)/ (2 * a) );
-    //     // console.log(root1, root2);
-    //     res.send("the root of the qiven equation is" + root1+ " and " + root2)
-       
-    // }else{
-    //     console.log("no real value");
-    //     res.send("no real value")
-    // }
 
 
 })
